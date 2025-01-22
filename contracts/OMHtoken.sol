44bc0d571eab1cdcb1049cd95d1fbd4421be3a8e
OmHawaldar <hawaldarom39@gmail.com>
2025-01-22 23:14:50 +0530

    intial commit on main

---
+++ contracts/OMHtoken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OMHtoken is ERC20 {
+
+    constructor() ERC20("OMH Token","OMH"){
+        _mint(msg.sender, 10000 * 10 ** decimals());
+
+    }
+
+}
